83aa36d9d7f94d8eaf4b0234cb9f69d21ef82d53
bxff <51504045+bxff@users.noreply.github.com>
2025-12-19 00:52:37 +0530

    fix: use parent repo node_modules

---
+++ example/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+    resolve: {
+        alias: {
+            '@codemirror/state': path.resolve(__dirname, 'node_modules/@codemirror/state'),
+            '@codemirror/view': path.resolve(__dirname, 'node_modules/@codemirror/view'),
+            '@codemirror/language': path.resolve(__dirname, 'node_modules/@codemirror/language'),
+            '@codemirror/commands': path.resolve(__dirname, 'node_modules/@codemirror/commands'),
+            '@lezer/common': path.resolve(__dirname, 'node_modules/@lezer/common'),
+            '@lezer/lr': path.resolve(__dirname, 'node_modules/@lezer/lr'),
+            '@lezer/markdown': path.resolve(__dirname, 'node_modules/@lezer/markdown'),
+        },
+    },
+    server: {
+        port: 8000,
+    },
+});
